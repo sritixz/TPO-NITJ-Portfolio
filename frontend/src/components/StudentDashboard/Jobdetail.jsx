77312035bbe05b480e77e109ae68c63119851cc0
Abhinav Sharma <abhisharma19091999@gmail.com>
2025-02-13 19:52:51 +0530

    Job details error resolved

--- frontend/src/components/StudentDashboard/Jobdetail.jsx
+++ frontend/src/components/StudentDashboard/Jobdetail.jsx
@@ -4,7 +4,7 @@ import { FaArrowLeft } from "react-icons/fa";
 import ApplicationForm from "./applicationform";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Swal from "sweetalert2";
-import {faMapMarkerAlt, faIndianRupee,faInfoCircle, faIdCard, faTasks, faTags, faUserTie} from '@fortawesome/free-solid-svg-icons';
+import {faMapMarkerAlt, faClipboardList,faInfoCircle, faBriefcase, faDollarSign, faTags, faUserTie} from '@fortawesome/free-solid-svg-icons';
 
 const Jobdetail = ({ job_id, onBack, onShow }) => {
     const [activeInfo, setActiveInfo] = useState("jobDescription");
