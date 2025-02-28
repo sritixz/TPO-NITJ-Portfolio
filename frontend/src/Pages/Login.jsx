cda1e566eee8e08c8cb8f7cb3d86652dc5e0aa1c
Puneet Khoiya <puneetkhoiyabnl8@gmail.com>
2025-02-28 21:51:31 +0530

    conflict

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -8,11 +8,7 @@ const login = () => {
   return (
     <>
       <Header />
-<<<<<<< HEAD
         <div className="max-w-7xl mx-auto pt-15 px-6 mt-20 mb-40">
-=======
-        <div className="max-w-7xl mx-auto pt-15 px-6 mt-12 mb-36">
->>>>>>> c39377ad0832ac0396f11c04fb33404f1c469ce4
         <Login Login={true}/>
       </div>
       <Footer />
--- frontend/src/Pages/Placement.jsx
+++ frontend/src/Pages/Placement.jsx
@@ -7,11 +7,7 @@ const Placement = () => {
   return (
     <>
       <Header />
-<<<<<<< HEAD
       <div className="mt-5"><Placements /></div>
-=======
-      <div className="mt-"><Placements /></div>
->>>>>>> c39377ad0832ac0396f11c04fb33404f1c469ce4
       <Footer/>
 
     </>
