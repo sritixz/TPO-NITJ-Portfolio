644cf8e0dc5aa8777f0b4b857f8c653655318dc5
Luson045 <lusonbasumatary2@gmail.com>
2025-03-29 09:44:19 +0530

    added recruiter management and student management

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -6,12 +6,30 @@ import {
   deleteJobProfile,
   bulkDeleteJobProfiles,
   toggleJobProfileVisibility,
+  getAllStudents,
+  updateStudentProfile,
+  deleteStudentProfiles,
+  addNewStudent,
+  getAllRecruiters,
+  updateRecruiterProfile,
+  deleteRecruiterProfiles,
+  addNewRecruiter
 } from "../controller/admin.js";
-
+//job profile routes
 router.get("/jobprofiles", getAllJobProfiles);
 router.put("/jobprofiles/:id", updateJobProfile);
 router.delete("/jobprofiles/:id", deleteJobProfile);
 router.post("/jobprofiles/bulk-delete", bulkDeleteJobProfiles);
 router.put("/jobprofiles/:id/toggle-visibility", toggleJobProfileVisibility);
+//student profile routes
+router.get("/students", getAllStudents);
+router.put("/students/:id",updateStudentProfile);
+router.post("/students",addNewStudent);
+router.delete("/students", deleteStudentProfiles);
+//recruiter profile routes
+router.get("/recruiters", getAllRecruiters);
+router.put("/recruiters/:id",updateRecruiterProfile);
+router.post("/recruiters",addNewRecruiter);
+router.delete("/recruiters", deleteRecruiterProfiles);
 
 export default router;
