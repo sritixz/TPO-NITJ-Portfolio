806719408c49a882290b99bdc45ce698fffdbf0d
sxnkush <saxenakushagra198@gmail.com>
2026-02-13 16:19:02 +0530

    Updated

--- backend/models/placement-registration.js
+++ backend/models/placement-registration.js
@@ -1,71 +1,90 @@
 import mongoose from "mongoose";
 
-const placementRegistrationSchema = new mongoose.Schema({
-  studentId:{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Student",
+const placementRegistrationSchema = new mongoose.Schema(
+  {
+    studentId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Student",
+    },
+    name: {
+      type: String,
+    },
+    rollno: {
+      type: String,
+    },
+    department: {
+      type: String,
+    },
+    course: {
+      type: String,
+    },
+    batch: {
+      type: String,
+    },
+    fatherName: {
+      type: String,
+    },
+    motherName: {
+      type: String,
+    },
+    category: {
+      type: String,
+    },
+    gender: {
+      type: String,
+    },
+    dateOfBirth: {
+      type: Date,
+    },
+    physicallyDisabled: {
+      type: Boolean,
+    },
+    disabilityType: {
+      type: String,
+    },
+    permanentAddress: {
+      type: String,
+    },
+    mobileNo: {
+      type: String,
+    },
+    emailNitj: {
+      type: String,
+    },
+    emailPersonal: {
+      type: String,
+    },
+    aadharCardNo: {
+      type: String,
+    },
+    interested: {
+      type: Boolean,
+    },
+    description: {
+      type: String,
+    },
+    trainingRequired: {
+      type: Boolean,
+      default: false,
+    },
+    privateType: {
+      type: String,
+    },
+    trainingPlatform: {
+      type: String,
+    },
+    preferredSector: {
+      type: String,
+    },
   },
-  name: {
-    type: String,
+  {
+    timestamps: true,
   },
-  rollno: {
-    type: String,
-  },
-  department: {
-    type: String,
-  },
-  course: {
-    type: String,
-  },
-  batch:{
-    type: String,
-  },
-  fatherName: {
-    type: String,
-  },
-  motherName: {
-    type: String,
-  },
-  category: {
-    type: String,
-  },
-  gender: {
-    type: String,
-  },
-  dateOfBirth: {
-    type: Date,
-  },
-  physicallyDisabled: {
-    type: Boolean,
-  },
-  disabilityType: {
-    type: String,
-  },
-  permanentAddress: {
-    type: String,
-  },
-  mobileNo: {
-    type: String,
-  },
-  emailNitj: {
-    type: String,
-  },
-  emailPersonal: {
-    type: String,
-  },
-  aadharCardNo: {
-    type: String,
-  },
-  interested: {
-    type: Boolean,
-  },
-  description: {
-    type: String,
-  },
-}, {
-  timestamps: true
-});
+);
 
-const PlacementRegistration = mongoose.model('PlacementRegistration', placementRegistrationSchema);
+const PlacementRegistration = mongoose.model(
+  "PlacementRegistration",
+  placementRegistrationSchema,
+);
 
 export default PlacementRegistration;
