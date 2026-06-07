277e1c8e1d4cd25b2b271e060e92ee68fcd03b91
Prabhleen-11 <prabhukaur011@gmail.com>
2026-06-07 19:15:08 +0530

    .

--- backend/routes/studentsuggestionroute.js
+++ backend/routes/studentsuggestionroute.js
@@ -1,10 +1,32 @@
+// import express from 'express';
+// import { savesuggestions,recentsuggestions,getContactedCompanies } from '../controller/suggestioncontroller.js';
+// const router = express.Router();
+
+// router.post("/suggestions",savesuggestions);
+// router.get("/recentsuggestions",recentsuggestions);
+// router.get("/contacted", getContactedCompanies);
+
+
+// export default router;
 import express from 'express';
-import { savesuggestions,recentsuggestions,getContactedCompanies } from '../controller/suggestioncontroller.js';
+import { 
+  savesuggestions, 
+  recentsuggestions, 
+  getContactedCompanies,
+  getsuggestions,      
+  updatesuggestions 
+} from '../controller/suggestioncontroller.js';
+
 const router = express.Router();
 
-router.post("/suggestions",savesuggestions);
-router.get("/recentsuggestions",recentsuggestions);
+router.post("/suggestions", savesuggestions);
+router.get("/recentsuggestions", recentsuggestions);
 router.get("/contacted", getContactedCompanies);
 
+
+router.get("/fetchsuggestions", getsuggestions); 
+
+
+router.patch("/updatesuggestion", updatesuggestions); 
 
 export default router;
